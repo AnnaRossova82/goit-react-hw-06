81725a8742c13c450f30bea2f96681f4dfc5a28b
AnnaRossova <rossova.anna@gmail.com>
2024-05-02 23:38:31 +0300

    cleaned

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -16,32 +16,9 @@ const filtersSlice = createSlice({
 
 export const { changeFilter } = filtersSlice.actions;
 
-// Функція для вибору значення пошукового терміну зі стану
 export const selectNameFilter = state => state.filters.name;
 
-// Функція для вибору значення пошукового терміну для експорту
 export const selectSearchTerm = state => state.filters.name;
 
 export default filtersSlice.reducer;
 
-
-/* import { createSlice } from '@reduxjs/toolkit';
-
-const initialState = {
-  name: '',
-};
-
-const filtersSlice = createSlice({
-  name: 'filters',
-  initialState,
-  reducers: {
-    changeFilter: (state, action) => {
-      state.name = action.payload;
-    },
-  },
-});
-
-export const { changeFilter } = filtersSlice.actions;
-export const selectNameFilter = state => state.filters.name;
-
-export default filtersSlice.reducer; */
