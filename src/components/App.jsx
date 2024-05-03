496531c364a5b1b47a48aa483605ae9e3c75a376
Anna Rossova <rossova.anna@gmail.com>
2024-05-03 15:24:40 +0300

    more corrections made

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { loadContacts, selectContacts } from '../redux/contactsSlice';
-import { selectNameFilter } from '../redux/filtersSlice'; 
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import SearchBox from './SearchBox/SearchBox';
@@ -9,16 +8,11 @@ import initialContacts from '../contacts.json';
 
 function App() {
   const contacts = useSelector(selectContacts);
-  const nameFilter = useSelector(selectNameFilter); // Оновлено імпорт та використання селектора
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadContacts(initialContacts));
   }, [dispatch]);
-
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(nameFilter.toLowerCase()) // Оновлено використання фільтра
-  );
 
   return (
     <div>
